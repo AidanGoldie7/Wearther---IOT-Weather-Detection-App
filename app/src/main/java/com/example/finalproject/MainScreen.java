3d64170ecb4e4a37cdbbea63bd8967c56b174337
aidangoldie7 <40402670@live.napier.ac.uk>
2021-05-14 06:22:01 +0100

    Eighth Commit - rest of app

--- app/src/main/java/com/example/finalproject/MainScreen.java
+++ app/src/main/java/com/example/finalproject/MainScreen.java
@@ -15,12 +15,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Objects;
-
 
 public class MainScreen extends AppCompatActivity {
     //Initialise variables
-    TextView temp, humidity;
+    TextView temp, humidity, air, uv, upperBodyUnder, upperBodyBase, upperBodyOuter, lowerBodyUnder, lowerBodyBase, waterproof, airResult, uvSunglasses;
     Button getDataButton;
     DatabaseReference ref;
 
@@ -30,10 +28,31 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        //set variables to txtViews and button
+
+        //set variables to txtViews
         temp = (TextView) findViewById(R.id.tempview);
         humidity = (TextView) findViewById(R.id.txtHum);
+        air = (TextView) findViewById(R.id.txtCO2);
+        uv = (TextView) findViewById(R.id.txtUV);
+
+
+        //validation txt boxes
+        upperBodyUnder = (TextView) findViewById(R.id.txtUpperUnderLayer);
+        upperBodyBase = (TextView) findViewById(R.id.txtUpperBaseLayer);
+        upperBodyOuter = (TextView) findViewById(R.id.txtUpperOuterLayer);
+
+        lowerBodyBase = (TextView) findViewById(R.id.txtLowerBaseLayer);
+        lowerBodyUnder = (TextView) findViewById(R.id.txtUpperUnderLayer);
+
+        waterproof = (TextView) findViewById(R.id.WeatherUpper);
+        airResult = (TextView) findViewById(R.id.txtAir);
+        uvSunglasses = (TextView) findViewById(R.id.txtUVRequired);
+
+
+
+        //button
         getDataButton = (Button) findViewById(R.id.btnGetData);
+
 
 
         //when button clicked
@@ -55,64 +74,126 @@ public class MainScreen extends AppCompatActivity {
                         for (DataSnapshot ds : snapshot.getChildren()) {
                             //gets value of item titled FireTemperature
                             String Temperature = (String) ds.child("FireTemperature").getValue();
-                            //gets value of item titles FireHUmidity
+                            //gets value of item titles FireHumidity
                             String Humidity = (String) ds.child("FireHumidity").getValue();
+                            //gets value of item titles FireAir
+                            String Air = (String) ds.child("FireAir").getValue();
+                            //gets value of item titled FireLight
+                            String Light = (String) ds.child("FireLight").getValue();
+
 
                             //sends these values to textViews which have been assigned to the above variables
-                            temp.setText("Temperature : " + Temperature);
-                            humidity.setText("Humidity : " + Humidity);
+                            temp.setText("Temperature : " + Temperature + "°C");
+                            humidity.setText("Humidity : " + Humidity + "%");
+                            air.setText("Air Quality : " + Air);
+                            uv.setText("Light Intensity : " + Light + " lux");
 
+
+
+
+                            double TempParse = Double.parseDouble(Temperature);
+                            double HumParse = Double.parseDouble(Humidity);
+                            double airParse = Double.parseDouble(Air);
+                            double lightParse = Double.parseDouble(Light);
+
+
+                            //Validation for Temperature
+                            if (TempParse<=0)
+                            {
+                                upperBodyUnder.setText("Under Layer : Thermal Second Skin");
+                                upperBodyBase.setText("Base Layer : Thermal T-shirt");
+                                upperBodyOuter.setText("Outer Layer : Insulated Jacket");
+
+                                lowerBodyUnder.setText("Thermal Underwear");
+                                lowerBodyBase.setText("Jogging Trousers");
+                            }
+                            if (TempParse >0 && TempParse <5)
+                            {
+                                upperBodyUnder.setText("Under Layer : None");
+                                upperBodyBase.setText("Base Layer : Thermal T-Shirt");
+                                upperBodyOuter.setText("Outer Layer : Light Jacket");
+
+
+                                lowerBodyUnder.setText("Under Layer : Running Leggings");
+                                lowerBodyBase.setText("Base Layer : Shorts");
+                            }
+                            if (TempParse >4 && TempParse <10)
+                            {
+                                upperBodyUnder.setText("Under Layer : None");
+                                upperBodyBase.setText("Base Layer : Long Sleeve T-Shirt");
+                                upperBodyOuter.setText("Outer Layer : Light Sweater");
+
+
+                                lowerBodyUnder.setText("Under Layer : None");
+                                lowerBodyBase.setText("Base Layer : Shorts");
+                            }
+                            if (TempParse >9 && TempParse <15)
+                            {
+                                upperBodyUnder.setText("Under Layer : None");
+                                upperBodyBase.setText("Base Layer : Long Sleeve T-Shirt");
+                                upperBodyOuter.setText("Outer Layer : None");
+
+
+                                lowerBodyUnder.setText("Under Layer : None");
+                                lowerBodyBase.setText("Base Layer : Shorts");
+                            }
+                            if (TempParse >14 && TempParse <20)
+                            {
+                                upperBodyUnder.setText("Under Layer : None");
+                                upperBodyBase.setText("Base Layer : T-Shirt");
+                                upperBodyOuter.setText("Outer Layer : None");
+
+
+                                lowerBodyUnder.setText("Under Layer : None");
+                                lowerBodyBase.setText("Base Layer : Shorts");
+                            }
+                            if (TempParse >19 && TempParse <25)
+                            {
+                                upperBodyUnder.setText("Under Layer : None");
+                                upperBodyBase.setText("Base Layer : Tank Top");
+                                upperBodyOuter.setText("Outer Layer : None");
+
+
+                                lowerBodyUnder.setText("Under Layer : None");
+                                lowerBodyBase.setText("Base Layer : Shorts");
+                            }
+
+
+                            //Humidity Validation
+                            if (HumParse > 50)
+                            {
+                                waterproof.setText("Waterproof Materials : YES");
+                            }
+
+                            if (HumParse <=50)
+                            {
+                                waterproof.setText("Waterproof Materials : NO");
+                            }
+
+
+                            //Air Quality Validation
+                            if (airParse >3000)
+                            {
+                                airResult.setText("Inhaler : YES");
+                            }
+                            if (airParse <3000)
+                            {
+                                airResult.setText("Inhaler : NO");
+                            }
+
+
+
+                            if(lightParse >999)
+                            {
+                                uvSunglasses.setText("Sunglasses : YES");
+                            }
+                            if(lightParse <1000)
+                            {
+                                uvSunglasses.setText("Sunglasses : NO");
+                            }
                         }
                     }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-            }
-        });
-
-        /*
-        getData.setOnClickListener(v -> {
-            ref = FirebaseDatabase.getInstance().getReference().child("FirebaseData").child("data").child("MZmoQyV65qhE2KF1xex");
-            ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                    String Temperature = snapshot.child("tempp").getValue().toString();
-                    temp.setText(Temperature);
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-        });
-
-        */
-
-    }
-}
-
-
-
-
-
-       /*
-        getData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ref = FirebaseDatabase.getInstance().getReference().child("FirebaseData").child("data").child("MZmoQyV65qhE2KF1xex");
-                ref.orderByKey().limitToLast(1).addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                        String FireTemperature = dataSnapshot.child("FireTemperature").getValue().toString();
-                        temp.setText(FireTemperature);
-                    }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
@@ -120,79 +201,11 @@ public class MainScreen extends AppCompatActivity {
                     }
                 });
             }
-
         });
     }
 }
-*/
 
 
 
 
 
-
-                //code working on
-              /*  EditText textUser = findViewById(R.id.txtUsername);
-                EditText textPassword = findViewById(R.id.txtPassword);
-
-                try {
-                    //ParticleCloudSDK.getCloud().logIn(textUser.getText().toString(), textPassword.getText().toString());
-                    ParticleCloudSDK.getCloud().logIn("aidangoldie7@gmail.com", "Password7");
-                    //Activity MainScreen;
-                    Toast.makeText(MainScreen.this, "logged in!", Toast.LENGTH_LONG).show();
-
-
-                    Intent HomeIntent = new Intent(MainScreen.this, HomeScreen.class);
-                    startActivity(HomeIntent);
-                    finish();
-
-
-                } catch (ParticleCloudException e) {
-                   // e.printStackTrace();
-                    Toast.makeText(MainScreen.this, "credentials wrong", Toast.LENGTH_LONG).show();
-                }*/
-
-                /*
-                try {
-                    ParticleCloudSDK.getCloud().logIn(textUser.getText().toString(), textPassword.getText().toString());
-                    Toaster.s(someActivity, "Logged in!");
-
-                    Toast.makeText(MainScreen.this, "logged in.",
-                            Toast.LENGTH_LONG).show();  //see below to find out more
-                } catch (ParticleCloudException e) {
-                    e.printStackTrace();
-                    Toast.makeText(MainScreen.this, "invalid",
-                            Toast.LENGTH_LONG).show();  //see below to find out more
-                }
-*/
-
-            //Toaster.s(someActivity, "Logged in!");
-                //End of code working on
-
-
-
-/*
-    Button buttonB = (Button)findViewById(R.id.button);
-        buttonB.setOnClickListener(new View.OnClickListener(){
-        //anonymous inner method called when buttonB clicked....
-        public void onClick(View v) {
-
-            List<ParticleDevice> devices = ParticleCloudSDK.getCloud().getDevices();
-            for (ParticleDevice device : devices) {
-                if (device.getName().equals("myDevice")) {
-                    //doSomethingWithMyDevice(device);
-                    break;
-                }
-            }
-
-
-            ListView myList = (ListView)findViewById(R.id.myList);
-            Log.d("FirstAppTag","onClick() called - scan device button");
-            Toast.makeText(MainScreen.this, "scanning for devices nearby.",
-                    Toast.LENGTH_LONG).show();  //see below to find out more
-            //tv1.setText(new Date().toString());
-        }
-
-    }
-}
-*/
